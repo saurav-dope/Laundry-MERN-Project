60d7f3f1559336c3503ce52c0441b2b4642895cd
Saurav Dutta <sauravdutta1270@gmail.com>
2022-06-08 00:05:00 +0530

    chenged credentials

--- backend/connection/connect.js
+++ backend/connection/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const main = async () => {
   await mongoose.connect(
-    'mongodb+srv://saurav:saurav@cluster0.v1c4j.mongodb.net/laundry-service?retryWrites=true&w=majority'
+    'mongodb+srv://<username>:<password>@cluster0.v1c4j.mongodb.net/laundry-service?retryWrites=true&w=majority'
   );
   console.log('Database connected');
 };
